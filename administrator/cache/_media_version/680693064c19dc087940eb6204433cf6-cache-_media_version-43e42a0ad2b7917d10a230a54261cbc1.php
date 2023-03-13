ca88d59e0f08dfa375a9e8eedbcc494dd1c17e14
martenmuhammad <102303552+martenmuhammad@users.noreply.github.com>
2023-03-13 09:38:19 +0700

    Initial commit

---
+++ administrator/cache/_media_version/680693064c19dc087940eb6204433cf6-cache-_media_version-43e42a0ad2b7917d10a230a54261cbc1.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"beb158e5b923f5e1aa9f69f142f22441";s:6:"output";s:0:"";}
---
+++ administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-03-06 02:56:36 UTC
+#Software: Joomla! 4.2.8 Stable [ Uaminifu ] 16-February-2023 15:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-03-06T02:56:36+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T02:56:53+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T02:57:26+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-13T01:12:08+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-13T01:12:37+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
